0f3bf1f0f025445870380191e93ab75fe08f7245
jaeryungchung <jaeryung.ch@gmail.com>
2025-10-19 06:31:24 +0900

    test6

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
+import babel from 'vite-plugin-babel';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import { reactRouter } from "@react-router/dev/vite";
+
+const ReactCompilerConfig = { /* ... */ };
+
 
 // Use root base for both dev and user-site deployment (user site serves from root)
 export default defineConfig({
@@ -13,3 +18,20 @@ export default defineConfig({
     }),
   ],
 })
+
+// export default defineConfig({
+//   base: '/',
+
+//   plugins: [
+//     reactRouter(),
+//     babel({
+//       filter: /\.[jt]sx?$/,
+//       babelConfig: {
+//         presets: ["@babel/preset-typescript"], // if you use TypeScript
+//         plugins: [
+//           ["babel-plugin-react-compiler", ReactCompilerConfig],
+//         ],
+//       },
+//     }),
+//   ],
+// });
