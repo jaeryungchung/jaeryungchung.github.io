d628928fbb6b76b5052cff0895e30b6e3e5797a9
jaeyoungchoi1 <choijy5847@naver.com>
2025-10-19 21:20:01 +0900

    please

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import babel from 'vite-plugin-babel';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { reactRouter } from "@react-router/dev/vite";
 
-const ReactCompilerConfig = { /* ... */ };
-const repoName = '/jaeryungchung.github.io/'
-
-
+// https://vite.dev/config/
 export default defineConfig({
-  // ✅ base 경로를 루트(/)로 설정
-  base: '/',
   plugins: [react()],
-});
+  base: '/',
+})
