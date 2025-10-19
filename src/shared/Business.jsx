7865cb55578bddd97484b7ca8fb11071fa54651d
jaeryungchung <jaeryung.ch@gmail.com>
2025-10-19 21:58:15 +0900

    business add

--- src/shared/Business.jsx
+++ src/shared/Business.jsx
@@ -6,12 +6,34 @@ export default function Business(){
       <h2>Field Business</h2>
       <div className="media-block">
         <div className="media-thumb">
-          <iframe src="https://www.youtube.com/embed/WBKB2V8waNU" title="SXSW Booth" frameBorder="0" allowFullScreen />
+          <iframe src="https://www.youtube.com/embed/WBKB2V8waNU" title="SXSW 2025 Booth" frameBorder="0" allowFullScreen />
         </div>
         <div className="media-text">
           <h3>SXSW 2025 Booth Management</h3>
           <p>📍 Austin, Texas, United States 🇺🇸</p>
           <p>Role: Participated as exhibitor for 'Mingle' booth from 'KAI Inc.' as part of KOCCA. Launched a private party for promotion with more than 300 participants.</p>
+        </div>
+      </div>
+
+      <div className="media-block">
+        <div className="media-thumb">
+          <iframe src="https://youtu.be/oexXZR-3hmQ?si=6l0xnO5wZ2v0zNrg" title="CES 2024 Booth" frameBorder="0" allowFullScreen />
+        </div>
+        <div className="media-text">
+          <h3>CES 2024 Mingle AI Booth Management</h3>
+          <p>📍 Las Vegas, United States 🇺🇸</p>
+          <p>Role: Managed promotion of Mingle AI at CES 2024. Prepared for merchandises and led the booth operation.</p>
+        </div>
+      </div>
+
+      <div className="media-block">
+        <div className="media-thumb">
+          <iframe src="https://youtu.be/0jr1cWUAZIQ" title="Worked as CoS (Chief of Staff) at KAI Inc." frameBorder="0" allowFullScreen />
+        </div>
+        <div className="media-text">
+          <h3>CES 2024 Mingle AI Booth Management</h3>
+          <p>📍 Seoul/Daejeon, Republic of Korea 🇰🇷</p>
+          <p>Role: Ran global and domestic business meetings for collaboration opportunities and sales.</p>
         </div>
       </div>
     </section>
