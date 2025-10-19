d38f2a115ae4b8a562ddfc496d3ee5518249f9cd
jaeyoungchoi1 <choijy5847@naver.com>
2025-10-19 21:05:02 +0900

    chore

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import ContactPage from './pages/ContactPage'
 
 export default function App(){
   return (
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/contact" element={<ContactPage/>} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/contact" element={<ContactPage/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-// App.jsx placeholder - project converted to static site
-// export default function App() {
-//   return null
-// }
