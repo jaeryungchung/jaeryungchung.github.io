2f29b71184ad37ca202098b7ffcb9784139cd9dc
jaeryungchung <jaeryung.ch@gmail.com>
2026-01-02 12:47:53 +0900

    cv-update

--- src/shared/Contact.jsx
+++ src/shared/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import cvPdf from '../assets/cv-20251011.pdf'
+import cv from '../assets/JaeryungChung_Resume_2026_Jan.pdf'
 
 export default function Contact(){
   return (
@@ -8,6 +9,7 @@ export default function Contact(){
       <p>Coffee Chat: <a href="https://calendar.app.google/zZtDZt9csPbR6SXp8" target="_blank">Schedule</a></p>
       <p>Email: <a href="mailto:jhyun513@kaist.ac.kr">jhyun513@kaist.ac.kr</a></p>
       <p>LinkedIn: <a href="https://www.linkedin.com/in/jaeryungchung" target="_blank">linkedin.com/in/jaeryungchung</a></p>
+      <p>CV: <a href={cv} target="_blank">CV PDF</a></p>
       {/* <p>CV: <a href={cvPdf} target="_blank">Download</a></p> */}
     </section>
   )
--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer(){
   return (
     <footer className="site-footer">
-      <small>© {new Date().getFullYear()} Jaeryung Chung   👀 Inspired by other amazing websites — updates coming soon. Until then, connect me on LinkedIn!</small>
+      <small> © {new Date().getFullYear()} Jaeryung Chung   👀 Inspired by other amazing websites — updates coming soon. Until then, connect me on LinkedIn!</small>
     </footer>
   )
 }
