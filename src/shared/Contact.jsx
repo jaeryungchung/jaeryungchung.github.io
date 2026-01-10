335ed3fd14d623cd10f83c37fc545681382d6ed3
jaeryungchung <jaeryung.ch@gmail.com>
2026-01-10 16:57:26 +0900

    add-google-scholar

--- src/shared/Contact.jsx
+++ src/shared/Contact.jsx
@@ -9,6 +9,8 @@ export default function Contact(){
       <p>Coffee Chat: <a href="https://calendar.app.google/zZtDZt9csPbR6SXp8" target="_blank">Schedule</a></p>
       <p>Email: <a href="mailto:jhyun513@kaist.ac.kr">jhyun513@kaist.ac.kr</a></p>
       <p>LinkedIn: <a href="https://www.linkedin.com/in/jaeryungchung" target="_blank">linkedin.com/in/jaeryungchung</a></p>
+      <p>Google Scholar: <a href=" https://scholar.google.com/citations?user=tQlsqHEAAAAJ&hl=ko" target="_blank">Profile Link</a></p>
+     
       <p>CV: <a href={cv} target="_blank">CV PDF</a></p>
       {/* <p>CV: <a href={cvPdf} target="_blank">Download</a></p> */}
     </section>
