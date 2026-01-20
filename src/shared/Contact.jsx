84962d3ee484fd63b5e46733e21440a1fe1b327b
jaeryungchung <jaeryung.ch@gmail.com>
2026-01-21 02:07:05 +0900

    portfolio

--- src/shared/Contact.jsx
+++ src/shared/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import cv from '../assets/JaeryungChung_Resume_20260121.pdf'
+import portfolio from '../assets/JaeryungChung_portfolio_0120_.pdf'
 
 export default function Contact(){
   return (
@@ -9,8 +10,9 @@ export default function Contact(){
       <p>Email: <a href="mailto:jhyun513@kaist.ac.kr">jhyun513@kaist.ac.kr</a></p>
       <p>LinkedIn: <a href="https://www.linkedin.com/in/jaeryungchung" target="_blank">linkedin.com/in/jaeryungchung</a></p>
       <p>Google Scholar: <a href=" https://scholar.google.com/citations?user=tQlsqHEAAAAJ&hl=ko" target="_blank">Profile Link</a></p>
-     
-      <p>CV: <a href={cv} target="_blank">CV PDF</a></p>
+      <p></p>
+      <p><a href={cv} target="_blank" className="document-link">CV PDF</a></p>
+      <p><a href={portfolio} target="_blank" className="document-link">Portfolio PDF</a></p>
       {/* <p>CV: <a href={cvPdf} target="_blank">Download</a></p> */}
     </section>
   )
